4b96931178cf6f541cbd5f28580e6c0c7d7cb495
steel25baby <bridgetgitonga73@gmail.com>
2024-05-27 16:50:04 +0300

    remove unncessary statements in the code

--- main.test.js
+++ main.test.js
@@ -7,16 +7,15 @@ expect(fizzBuzz(6)).toBe("6Fizz");
 expect(fizzBuzz(9)).toBe("9Fizz");
 })
 
-describe('fizzBuzz function tests', () => {
     test('should return FizzBuzz for numbers divisible by both 3 and 5', () => {
         expect(fizzBuzz(15)).toBe("15FizzBuzz")
     })
    
-})
-describe('fizzBuzz function tests', () => {
+
+
     test('should return Buzz for numbers divisible by  5', () => {
         expect(fizzBuzz(40)).toBe("40Buzz")
     })
-})
+
 
 })
